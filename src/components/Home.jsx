acfb389d03e99942ae988f168dc73b3b9509e001
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-04-20 12:05:35 +0700

    Search bar UI

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
+import HeroSection from './HeroSection'
 
 const Home = () => {
   return (
     <div>
         <Navbar />
+        <HeroSection/>
+        {/* <CategoryCarousel/>
+        <LastestJobs/>
+        <Footer/> */}
     </div>
   )
 }
