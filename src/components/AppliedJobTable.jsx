e2adb32d17000b732a26d95d97bb213588199c4f
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-04-23 09:53:10 +0700

    table applied Jobs ui

--- src/components/AppliedJobTable.jsx
+++ src/components/AppliedJobTable.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from './ui/table'
+import { Badge } from './ui/badge'
 
 const AppliedJobTable = () => {
   return (
-    <div>AppliedJobTable</div>
+    <div>
+        <Table>
+            <TableCaption>A List of Applied Jobs</TableCaption>
+            <TableHeader>
+                <TableRow>
+                    <TableHead>Date</TableHead>
+                    <TableHead>Job Role</TableHead>
+                    <TableHead>Company</TableHead>
+                    <TableHead className='text-right'>Status</TableHead>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+                {
+                    [1,2,3,4].map((item, index) => (
+                        <TableRow key={index} className='hover:bg-gray-100 cursor-pointer'>
+                            <TableCell className='text-left'>2023-10-01</TableCell>
+                            <TableCell className='text-left'>Software Engineer</TableCell>
+                            <TableCell className='text-left'>ABC Company</TableCell>
+                            <TableCell className='text-right'><Badge>Selected</Badge></TableCell>
+                        </TableRow>
+                    ))
+                }
+            </TableBody>
+        </Table>
+    </div>
   )
 }
 
