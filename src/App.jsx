9bf0f3c3c143bc1c01dea156e31beb3b6fc888c7
nguyendungnd204 <ngvandungnvd204@gmail.com>
2025-04-30 15:50:26 +0700

    UI Home admin

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import Jobs from './components/Jobs'
 import Browse from './components/Browse'
 import Profile from './components/Profile'
 import JobDescription from './components/JobDescription'
+import Companies from './components/admin/Companies'
+import CreateCompany from './components/admin/CreateCompany'
 
 const appRouter = createBrowserRouter([
   {
@@ -36,6 +38,16 @@ const appRouter = createBrowserRouter([
   {
     path: '/profile',
     element: <Profile/>
+  },
+
+  //admin
+  {
+    path: "/admin/companies",
+    element: <Companies/>
+  },
+  {
+    path: "/admin/companies/create",
+    element: <CreateCompany/>
   }
  
 ]);
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -79,10 +79,14 @@ const Navbar = () => {
                     </div>
                   </div>
                   <div className='flex flex-col my-2  text-gray-600'>
-                    <div className='flex w-fit items-center gap-2 cursor-pointer'>
-                      <User2 />
-                      <Button variant='link'> <Link to='/profile'>View Profile</Link></Button>
-                    </div>
+                    {
+                      user && user.role == 'jobseeker' && (
+                        <div className='flex w-fit items-center gap-2 cursor-pointer'>
+                          <User2 />
+                          <Button variant='link'> <Link to='/profile'>View Profile</Link></Button>
+                        </div>
+                      )
+                    }
                     <div className='flex w-fit items-center gap-2 cursor-pointer'>
                       <LogOut />
                       <Button onClick={logoutHandler} variant='link'>Logout</Button>
